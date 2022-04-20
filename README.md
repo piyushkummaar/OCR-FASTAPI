96ed1979069df3db040b821ae0a70f7889fdf7dd
piyushkummaar <pshkmr007@gmail.com>
2022-04-20 20:46:14 +0530

    adding

--- README.md
+++ README.md
@@ -1,9 +1,65 @@
 # OCR-FASTAPI
 OCR FAST API PRACTICE CODE
 
+Make sure you have python install in you system
+
+# For windows
+
+## Install tesseract for windows 
+
+    https://codetoprosper.com/tesseract-ocr-for-windows/
+
 
 ## To run the code
 
-install requirements 
+### Install requirements using command 
 
-and run  the modelrestapiV1.py file
+    pip install -r requirements.txt  
+
+###  Now run the main file  
+    
+    python modelrestapiV1.py
+
+### Now it will start serving on you localhost
+
+    1. For swagger go to this url 
+
+    http://127.0.0.1:8000/docs
+
+    1. For devloper documentation go to this url 
+
+    http://127.0.0.1:8000/redoc
+
+
+# For ubuntu
+
+## Install tesseract for ubuntu
+
+    https://linuxhint.com/install-tesseract-ocr-linux/#:~:text=To%20install%20Tesseract%20on%20Debian,shown%20in%20the%20screenshot%20below.&text=This%20will%20install%20Tesseract%20under,install%20the%20English%20language%20pack.
+
+### Now Install opencv  
+
+    sudo apt update
+    sudo apt install python3-opencv
+
+
+## To run the code
+
+### Install requirements using command 
+
+    pip install -r requirements.txt  
+
+###  Now run the main file  
+    
+    python modelrestapiV1.py
+
+
+### Now it will start serving on you localhost
+
+    1. For swagger go to this url 
+
+    http://127.0.0.1:8000/docs
+
+    1. For devloper documentation go to this url 
+
+    http://127.0.0.1:8000/redoc
--- ocr_scriptV1.py
+++ ocr_scriptV1.py
@@ -8,6 +8,7 @@ from PIL import Image
 import cv2
 import numpy as np
 import sys
+import re
 
 class IMAGETOTEXT:
     def __init__(self):
@@ -19,21 +20,37 @@ class IMAGETOTEXT:
             pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
         else:
             pytesseract.pytesseract.tesseract_cmd = r"usr/bin/tesseract"
-    def dodgeV2(self,x,y):
-        return cv2.divide(x,255-y,scale=256)
-        
 
     def extracttext(self,image):
         img = cv2.imread(image)
         img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         img_invert = cv2.bitwise_not(img_gray) #gray scale to inversion of the image
-        lines = pytesseract.image_to_string(img_invert, lang='eng')
+
+        ######################
+        # Binarized image
+        th, img_th = cv2.threshold(img_invert, 128, 255, cv2.THRESH_BINARY)
+
+        kernel = np.ones((5,5), np.uint8)       
+        # The first parameter is the original image,
+        # kernel is the matrix with which image is
+        # convolved and third parameter is the number
+        # of iterations, which will determine how much
+        # you want to erode/dilate a given image.
+        # img_erosion = cv2.erode(img_th, kernel, iterations=1)
+        img_dilation = cv2.dilate(img_invert, kernel, iterations=1)
+
+        lines = pytesseract.image_to_string(img_dilation, lang='eng')
         lines = lines.split("\n")
+        lines = list(filter(None, lines))
         count = 1
         extract = dict()
         
         for text in lines:
-            if len(text) > 0 or text != "  ":
+            if len(text) > 0 or text !="  ":
                 extract[f"line {count}"] = text
             count += 1
         return extract 
+
+
+# main = IMAGETOTEXT()
+# print(main.extracttext('Sample-images/image_picker_2F5070F1-A609-4DAD-B378-F1216DD6B2BE-12309-000005D9E4121B16.jpg'))
